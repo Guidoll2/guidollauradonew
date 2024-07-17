6e604aff5a518c2b051049b36470af44cc8de5a9
Guido Llaurado <guido.llaurado@gmail.com>
2024-07-17 18:28:19 -0300

    New layout for emplearg

--- src/app/emplearg/page.tsx
+++ src/app/emplearg/page.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 export default function Home() {
    return (
          
-      <div className="flex flex-col h-screen w-screen bg-gradient-to-b from-blue-400 via-white to-blue-400 items-center justify-center" id="">
+      <div className="flex flex-col h-screen w-screen bg-black items-center justify-center" id="">
 
-     <Image src="/Full-no-bg.png" alt="empleargpicture" width={1000} height={1000} />
+     <Image src="/Full-no-bg.png" alt="empleargpicture" width={1000} height={1000} className="w-1/4" />
     
     <div className="flex flex-col items-center">
      
-     <p className="text-6xl text-black ">Sitio en construcción, seguinos en Instagram!</p>
+     <p className="text-3xl text-white ">Sitio en construcción, seguinos en Instagram!</p>
      <a href="https://www.instagram.com/emple_arg/" target="_blank">
-     <Image src='/instagram.svg' className="w-12 h-12 mt-8" height={1000} width={1000} alt="iglogo"></Image></a>
+     <Image src='/instagramgray200.png' className="w-12 h-12 mt-8" height={1000} width={1000} alt="iglogo"></Image></a>
      </div>    
       
       </div>

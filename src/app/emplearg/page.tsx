096d54414aa1e2b4842706e5db08f57f3295eff7
Guido Llaurado <guido.llaurado@gmail.com>
2024-07-17 18:37:18 -0300

    Fixing layout

--- src/app/emplearg/page.tsx
+++ src/app/emplearg/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
          
       <div className="flex flex-col h-screen w-screen bg-black items-center justify-center" id="">
 
-     <Image src="/Full-no-bg.png" alt="empleargpicture" width={1000} height={1000} className="w-1/4" />
+     <Image src="/Full-no-bg.png" alt="empleargpicture" width={1000} height={1000} className="" />
     
     <div className="flex flex-col items-center">
      
-     <p className="text-3xl text-white ">Sitio en construcción, seguinos en Instagram!</p>
+     <p className="text-3xl text-white text-center ">Sitio en construcción, seguinos en Instagram!</p>
      <a href="https://www.instagram.com/emple_arg/" target="_blank">
      <Image src='/instagramgray200.png' className="w-12 h-12 mt-8" height={1000} width={1000} alt="iglogo"></Image></a>
      </div>    
